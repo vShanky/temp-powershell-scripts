15a80f0ac5c510159a5e5e09248e2115b9eef125
Shashank Tijare <90565017+vShanky@users.noreply.github.com>
2025-11-20 17:16:08 +0530

    Update Create-AdminUser.ps1

--- Create-AdminUser.ps1
+++ Create-AdminUser.ps1
@@ -1,16 +1,24 @@
-# This will create an user with admin priviledges in windows , Run as Administrator
+# This will create an Admin user in windows, Run as Administrator
 
-# Variables (edit these)
-$AdminUser = "adminuser"
-$AdminPassword = "Adm!nPass123"
+#powershell.exe -ExecutionPolicy Bypass -File Create-AdminUser.ps1 -AdminUser "adminuser" -AdminPassword "Adm!nPass123"
+
+#Get-LocalUser  --  It shows the list of users
+
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$AdminUser,
+
+    [Parameter(Mandatory=$true)]
+    [string]$AdminPassword
+)
 
 # Convert password to secure string
 $SecurePassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
 
 # Create user account
-New-LocalUser -Name $AdminUser -Password $SecurePassword -FullName "Administrator User" -Description "Admin local user account"
+New-LocalUser -Name $AdminUser -Password $SecurePassword -FullName $AdminUser -Description "Administrator local user account" -ErrorAction Stop
 
 # Add user to Administrators group
-Add-LocalGroupMember -Group "Administrators" -Member $AdminUser
+Add-LocalGroupMember -Group "Administrators" -Member $AdminUser -ErrorAction Stop
 
-Write-Host "Administrator user '$AdminUser' created successfully!"
+Write-Host "Administrator user '$AdminUser' created successfully!" -ForegroundColor Green
